60b9666b92f0cc6920e9895d6bd0aa87ff3d88ee
Xelanor <berkeozelsel@gmail.com>
2020-07-01 09:16:17 +0300

    Auth frontend side

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-  return <div className="App"></div>;
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./containers/auth/Register";
+import Login from "./containers/auth/Login";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
-
 export default App;
