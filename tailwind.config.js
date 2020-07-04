cb25aa34ccc79f3f192678d53db6e98a43efddef
Xelanor <berkeozelsel@gmail.com>
2020-07-04 17:34:07 +0300

    Tailwind css Added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        sigortes: "#12B2BA",
+        sigortes2: "#FB7540",
+        sigortes3: "#7F8384",
+        sigortes4: "#FFFFFF",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
